3943fe188ce83f69dfaf72a0c4e69767f8a2323f
ratamir <ratamir@redhat.com>
2020-02-27 07:54:38 +0200

    Add 'date' to gss closed loop
    Signed-off-by: ratamir <ratamir@redhat.com>

--- gss_closed_loop.py
+++ gss_closed_loop.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from helpers import *
+from datetime import datetime
 
 g = gapi.GoogleSpreadSheetAPI(SPREADSHEET_NAME, "GSS closed loop")
+now = datetime.today()
 
 open_gss = len(get_gss_closed_loop("qe_test_coverage?"))
 acked_gss = len(get_gss_closed_loop("qe_test_coverage+"))
 naked_gss = len(get_gss_closed_loop("qe_test_coverage-"))
 
-g.insert_row([open_gss, acked_gss, naked_gss])
+g.insert_row([now.strftime("%Y-%m-%d"), open_gss, acked_gss, naked_gss])
